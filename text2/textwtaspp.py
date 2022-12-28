55ceed3dfbf96a1f8acab4f5acbde00973cf333c
kavithaboda <93815649+kavithaboda@users.noreply.github.com>
2022-12-28 15:19:15 +0530

    Delete textwtaspp.py

--- text2/textwtaspp.py
+++
@@ -1,9 +0,0 @@
-# importing the module
-import pywhatkit
-# using Exception Handling to avoid
-# unprecedented errors
-try:
-    pywhatkit.sendwhatmsg("+917730949541","Hello",22,28)
-    print("Successfully Sent!")
-except:
-    print("An Unexpected Error!")

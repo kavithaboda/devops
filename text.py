62fdefc52dbfe7c4ba3e7280d6bd868a6257c235
kavithaboda <93815649+kavithaboda@users.noreply.github.com>
2022-12-28 15:18:48 +0530

    Delete text.py

--- text.py
+++
@@ -1,9 +0,0 @@
-import random
-import pyautogui as pg
-import time
-words = ('dumb','donkey','stupid')
-time.sleep(8)
-for i in range(100):
-    a = random.choice(words)
-    pg.write("you are a" + a)
-    pg.press('enter')
